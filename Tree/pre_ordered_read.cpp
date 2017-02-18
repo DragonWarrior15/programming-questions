8dcfc8053546fadc7234edfe3a5c146e9e3c0110
DragonWarrior15 <DragonWarrior15@users.noreply.github.com>
2017-02-18 21:16:27 +0530

    Add files via upload

---
+++ Tree/pre_ordered_read.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+long left_child[26], right_child[26]; col[26]
+int main(){
+  int t, C, X, i;
+  char c[10000];
+  stack<int> s;
+  scanf(" %d", &t);
+  while(t--){
+    i = 10000;
+    while(i--){
+      c[i] = '1';
+    }
+    std::fill(left_child, left_child + 26, 0);
+    std::fill(right_child, right_child + 26, 0);
+    std::fill(col, col + 26, 0);
+    scanf(" %d", &C);
+    scanf("%s\n", &c);
+    // cout<<c<<"\n";
+    C += 10000
+    
+  }
+  return 0;
+}
