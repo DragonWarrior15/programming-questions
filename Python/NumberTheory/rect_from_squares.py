d69db501a09c16627b131302cd494e9823046d20
Vaibhav Ojha <DragonWarrior15@users.noreply.github.com>
2018-06-24 13:46:39 +0530

    Create rect_from_squares.py
    get count of rect from given no of squares

---
+++ Python/NumberTheory/rect_from_squares.py
@@ -0,0 +1,26 @@
+'''
+Given S squares, what is the total no of unique rectangles possible,
+using some or all of them, rectangle that can be obtained from rotation of
+another should not be counted again
+'''
+from sys import stdin
+
+T = int(stdin.readline().strip())
+data = list(map(int, stdin.readlines()))
+
+square_dict = {}
+
+for S in data:
+    ans = 0
+    for i in range(1, S + 1):
+        try:
+            ans += square_dict[i]
+        except KeyError:
+            square_dict[i] = 0
+            # calculate square root, perfect squares and odd nos will be special cases
+            root = int(i ** 0.5)
+            for j in range(1, root + 1):
+                if(i%j == 0):
+                    square_dict[i] += 1
+            ans += square_dict[i]
+    print(ans)
